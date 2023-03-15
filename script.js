91f6cf4bcc260e0f4ca082cc2cb2c3daf92e226f
jemarval <jemarval@gmail.com>
2023-03-15 16:36:37 -0300

    add sidebar menu

---
+++ script.js
@@ -0,0 +1,22 @@
+  let menubtn = document.getElementById('menubtn');
+  let mySidebar = document.getElementById('mySidebar');
+  let main = document.getElementById('main');
+
+  let openNav = () => {
+    mySidebar.style.left = '0';
+    main.style.left = '20rem';
+    menubtn.innerHTML = "&#10005;"
+    
+    menubtn.addEventListener('click', closeNav)
+    menubtn.removeEventListener('click', openNav)
+  }
+
+  menubtn.addEventListener('click', openNav)
+
+  let closeNav = () => {
+    mySidebar.style.left = '-20rem';
+    main.style.left = '0';
+    menubtn.innerHTML = '&#9776;'
+    menubtn.removeEventListener('click', closeNav)
+    menubtn.addEventListener('click', openNav)
+  }
